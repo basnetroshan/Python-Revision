d0bf975b639d3d4c5fc75ec13a35d25d01b66a00
Roshan Basnet <basnet.roshan1991@gmail.com>
2020-02-05 17:02:42 +1100

    Variable Declaration and Comment

--- Syntax.py
+++ Syntax.py
@@ -1,2 +1,16 @@
 if 5 > 2:
     print("Five is greater than two!")
+
+#Variable declaration
+x = 5
+y = "Hello World"
+
+print(x)
+print(y)
+
+#Comments [Python uses # or 
+"""
+this is a comment
+written in more than one line
+"""
+print("Hello World!")
