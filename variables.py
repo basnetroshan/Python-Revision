0348011c8c601a9801cf7b0b96f53530594dbc39
Roshan Basnet <basnet.roshan1991@gmail.com>
2020-02-06 12:37:15 +1100

    More on variables

--- variables.py
+++ variables.py
@@ -8,3 +8,29 @@ print(y)
 
 a = "Basnet" # is the same as
 a = 'Basnet'
+
+# Assigning value to multiple variables
+p, q, r = "Mango" , "Banana" , "Peach"
+print(p)
+print(q)
+print(r)
+
+#Assigning same value to multiple variables
+e = f = g = "Orange"
+print(e)
+print(f)
+print(g) 
+
+#using + character to combine both text and a variable
+s = "going good,"
+t = ' well done!'
+print("Revision is " +s +t)
+
+# + for numbers
+first_num = second_num = 6
+print(first_num + second_num )
+
+#Error if string and number is combiled
+''' x= 5
+    y = "Roshan"
+    print(x + y) '''
