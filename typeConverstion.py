6f4fff429a5caa6c14b1152c29aaa60fab419b04
Roshan Basnet <basnet.roshan1991@gmail.com>
2020-02-06 13:52:22 +1100

    Data type conversion

---
+++ typeConverstion.py
@@ -0,0 +1,23 @@
+x = 1 #int
+y = 2.8 #float
+z = -5j #complex
+
+#Convert from int to float
+a = float(x)
+
+#Convert from float to int
+b = int(y)
+
+#Convert from int to complex
+c = complex(z)
+
+print(a)
+print(b)
+print(c)
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
