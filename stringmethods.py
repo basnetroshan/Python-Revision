e5a2a35a3c55241e2db7b777bbe8bd4668955a12
Roshan Basnet <basnet.roshan1991@gmail.com>
2020-02-10 17:17:39 +1100

    capitalize and casefold functions

---
+++ stringmethods.py
@@ -0,0 +1,5 @@
+txt = "i am rosHan"
+print(txt.capitalize()) #Capitalises the first letter of the string
+
+txt = "Hello, And Welcome To My World!"
+print(txt.casefold()) #Converts string into lower case
