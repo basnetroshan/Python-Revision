6a34ebb043ef0a7789a1490d1db04c3a4df79f01
Roshan Basnet <basnet.roshan1991@gmail.com>
2020-02-11 17:12:51 +1100

    center and count methods

--- stringmethods.py
+++ stringmethods.py
@@ -3,3 +3,10 @@ print(txt.capitalize()) #Capitalises the first letter of the string
 
 txt = "Hello, And Welcome To My World!"
 print(txt.casefold()) #Converts string into lower case
+
+txt = "roshan"
+print(txt.center(20,'x')) #center align the string, using a specified character (space is default) as the fill character.
+
+txt = "I love apples, apple are my favorite fruit"
+x = txt.count("a", 0, 24)
+print(x)
